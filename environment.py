b65f7a48c3268627499e99686e07fbf0b6125ff0
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2024-04-30 20:17:09 -0400

    Starting classes

---
+++ environment.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Environment:
+    def __init__(self, occupancy_data: str | np.ndarray):
+        if type(occupancy_data) is str:
+            self.occupancy_grid = Environment.read_from_file(occupancy_data)
+        else:
+            self.occupancy_grid = occupancy_data
+
+
+    def read_from_file(image_filename):
+        pass
+
+    def is_occupied(self):
+        pass
