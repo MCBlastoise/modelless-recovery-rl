65c8ca9900531f98fa1b310bd045882e1ae92006
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2024-05-09 21:16:44 -0400

    Changed agent to use trajectory to nearest unexplored thing instead of just exploring aimlessly. Also added PDM update to things in latest walked path.

--- agent.py
+++ agent.py
@@ -7,6 +7,7 @@ from enum import Enum
 class Agent:
     EPSILON = 0.7
     PDM_UPDATE_DELTA = 0.2
+    PATH_DANGER_WINDOW = 4
     
     def __init__(self, initial_pdm, initial_coords = (0, 0)):
         self.pdm = initial_pdm # numpy array
@@ -16,9 +17,9 @@ class Agent:
         self.update_explored(initial_coords)
 
         self.trajectory = None
-        # self.plan_state = False
-        trajectory = self.get_new_trajectory()
-        print(trajectory)
+        self.trajectory_step = None
+
+        self.previous_positions = [None] * self.PATH_DANGER_WINDOW
 
     def get_new_trajectory(self):
         queue = [ (self.pos,) ]
@@ -31,7 +32,7 @@ class Agent:
             for neighbor in neighbors:
                 new_path = path + (neighbor,)
                 if goal_func(neighbor):
-                    return new_path
+                    return list(new_path[1:])
                 if neighbor not in seen:
                     seen.add(neighbor)
                     queue.append(new_path)
@@ -40,40 +41,35 @@ class Agent:
         """
         returns action that agent should execute - either task policy or recovery policy
         """
-        possible_next_coords = self.get_next_coordinates()
 
-        # print("coord options", possible_next_coords)
-        task_action = self.task_policy(possible_next_coords)
+        task_action = self.task_policy()
 
         # Decide if safe, if not get recovery action
         if self.is_safe(task_action):
-            print("Took task action", task_action)
+            # print("Took task action", task_action)
+            self.trajectory_step += 1
             return task_action
 
+        # Failure, so invalidate trajectory
+        self.invalidate_trajectory()
+
+        possible_next_coords = self.get_next_coordinates()
         recovery_action = self.recovery_step(possible_next_coords)
-        print("Took recovery", recovery_action)
+        # print("Took recovery", recovery_action)
 
         return recovery_action
 
-    def task_policy(self, possible_next_coords):
+    def task_policy(self):
         """
         policy to poll for desired action
         """
-        step = possible_next_coords[0]
-        unexplored_coords, explored_coords = [], []
 
-        for coord in possible_next_coords:
-            if not self.explored[*coord]:
-                unexplored_coords.append(coord)
-            else:
-                explored_coords.append(coord)
-        
-        if unexplored_coords:
-            step = random.choice(unexplored_coords)
-        elif explored_coords:
-            step = random.choice(explored_coords)
-        
-        return step
+        if self.trajectory is None or self.trajectory_step >= len(self.trajectory):
+            self.trajectory = self.get_new_trajectory()
+            self.trajectory_step = 0
+
+        task_action = self.trajectory[self.trajectory_step]
+        return task_action
 
     def recovery_step(self, possible_next_coords) -> tuple:
         """
@@ -106,21 +102,48 @@ class Agent:
                     continue
                 next_coordinates.append( (new_r, new_c) )
         
+        random.shuffle(next_coordinates)
         return next_coordinates
 
     def get_next_coordinates(self):
         return self.possible_steps(self.pos)
 
+    def update_danger_zone(self):
+        valid_coords = [ coord for coord in self.previous_positions if coord is not None ]
+        for coord in valid_coords:
+            print("Updating coord", coord, "with danger")
+            self.update_pdm(coord, obstacle=True)
+
+    def invalidate_trajectory(self):
+        self.trajectory = None
+        self.trajectory_step = None
+
+    def reset_for_failure(self, coords):
+        self.update_danger_zone()
+        self.trajectory, self.trajectory_step = None, None
+        self.update_position(coords)
+
+    def successful_move(self, coords):
+        # raise NotImplementedError
+        self.update_position(coords)
+        self.update_pdm(self.pos, False)
+
     def update_position(self, coords):
         self.pos = coords
         self.update_explored(coords)
+
+        self.previous_positions.pop(0)
+        self.previous_positions.append(self.pos)
+
+        # print(self.previous_positions)
 
     def update_explored(self, coords):
         self.explored[*coords] = 1
 
     def update_pdm(self, coords: tuple[int, int], obstacle: bool):
-        delta = self.PDM_UPDATE_DELTA if obstacle else -self.PDM_UPDATE_DELTA
-        self.pdm[*coords] = max(min(self.pdm[*coords] + delta, 1.0), 0.0)
+        # delta = self.PDM_UPDATE_DELTA if obstacle else -self.PDM_UPDATE_DELTA
+        # self.pdm[*coords] = max(min(self.pdm[*coords] + delta, 1.0), 0.0)
+        self.pdm[*coords] = int(obstacle)
 
     def get_probability_obstacle(self, coords: tuple[int, int]):
         return self.pdm[*coords]
